8750a03b2215f0ae2827e58801557c589512996a
haoxinya <haoxinya@babyfs.cn>
2019-05-08 09:48:03 +0800

    地址

--- components/plugins/picker.js
+++ components/plugins/picker.js
@@ -1,114 +1,42 @@
 import React from 'react';
-import List from 'antd-mobile/lib/list'; // 加载 JS
-import 'antd-mobile/lib/list/style/css'; // 加载 CSS
 import Picker from 'antd-mobile/lib/picker'; // 加载 JS
 import 'antd-mobile/lib/picker/style/css'; // 加载 CSS
-import { createForm } from 'rc-form';
-import arrayTreeFilter from 'array-tree-filter';
-import { district, provinceLite } from 'antd-mobile-demo-data';
+import {
+  createForm,
+} from 'rc-form';
+import area from '../../utils/area';
+import './picker.less';
 
+const CustomChildren = ({ onClick, children, extra }) => (
+  <div
+    onClick={onClick}
+    style={{ backgroundColor: '#fff', paddingLeft: 15, paddingRight: 15 }}
+  >
+    <div className="test" style={{ display: 'flex', height: '45px', alignItems: 'center', borderBottomWidth: '1px', borderBottomColor: '#ccc', borderBottomStyle: 'solid' }}>
+      <div style={{ flex: 3, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{children}</div>
+      <div style={{ flex: 7, textAlign: 'right', color: '#888', marginRight: 15 }}>{extra}</div>
+    </div>
+  </div>
+);
 
 class Test extends React.Component {
   state = {
     data: [],
-    cols: 1,
     pickerValue: [],
-    asyncValue: [],
-    sValue: ['2013', '春'],
-    visible: false,
-    colorValue: ['#00FF00'],
-  };
-
-  onClick = () => {
-    setTimeout(() => {
-      this.setState({
-        data: provinceLite,
-      });
-    }, 120);
-  };
-
-  onPickerChange = (val) => {
-    console.log(val);
-    let colNum = 1;
-    const data = this.state;
-    const d = [...data];
-    const asyncValue = [...val];
-    if (val[0] === 'zj') {
-      d.forEach((i) => {
-        if (i.value === 'zj') {
-          colNum = 2;
-          if (!i.children) {
-            i.children = [{
-              value: 'zj-nb',
-              label: '宁波',
-            }, {
-              value: 'zj-hz',
-              label: '杭州',
-            }];
-            asyncValue.push('zj-nb');
-          } else if (val[1] === 'zj-hz') {
-            i.children.forEach((j) => {
-              if (j.value === 'zj-hz') {
-                j.children = [{
-                  value: 'zj-hz-xh',
-                  label: '西湖区',
-                }];
-                asyncValue.push('zj-hz-xh');
-              }
-            });
-            colNum = 3;
-          }
-        }
-      });
-    } else {
-      colNum = 1;
-    }
-    this.setState({
-      data: d,
-      cols: colNum,
-      asyncValue,
-    });
-  };
-
-  getSel() {
-    const { pickerValue } = this.state;
-    if (!pickerValue.value) {
-      return '';
-    }
-    const treeChildren = arrayTreeFilter(district, (c, level) => c.pickerValue.value === pickerValue.value[level]);
-    return treeChildren.map(v => v.pickerValue.label).join(',');
-  }
-  // setVal() {
-  //   this.props.form.setFieldsValue({
-  //     district: ['340000', '340800', '340822'],
-  //   });
-  // },
-
-  onChangeColor = (color) => {
-    this.setState({
-      colorValue: color,
-    });
   };
 
   render() {
-    const { form } = this.props;
+    const { pickerValue } = this.state;
     return (
-      <div>
-        <List style={{ backgroundColor: 'white' }} className="picker-list">
-          <Picker
-            extra="请选择(可选)"
-            data={district}
-            title="Areas"
-            {...form.getFieldProps('district', {
-              initialValue: ['340000', '341500', '341502'],
-            })}
-            onOk={e => console.log('ok', e)}
-            onDismiss={e => console.log('dismiss', e)}
-          >
-            <List.Item arrow="horizontal">选择地址</List.Item>
-          </Picker>
-        </List>
-      </div>
+      <Picker
+        title="选择地区"
+        data={area}
+        value={pickerValue}
+        onChange={v => this.setState({ pickerValue: v })}
+        onOk={v => this.setState({ pickerValue: v })}
+      >
+        <CustomChildren>选择地址</CustomChildren>
+      </Picker>
     );
   }
 }
--- src/pages/address/addAddress.js
+++ src/pages/address/addAddress.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './addAddress.less';
+import NavBar from 'components/plugins/navbar';
 import Picker from '../../../components/plugins/picker';
 
 class AddAddress extends Component {
   render() {
     return (
-      <div>
-        新增收货地址
+      <div className="address-addedit-wrap">
+        <NavBar title="添加收货地址" icon="" />
+        <p><label>姓名</label><input type="text" /></p>
+        <p><label>手机号码</label><input type="text" /></p>
         <Picker />
+        <p><label className="noborder">详细地址</label><textarea name="address" maxLength="50" /></p>
       </div>
     );
   }
